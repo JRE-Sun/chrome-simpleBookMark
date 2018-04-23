7b95e58e8e9391d2fdbc8d2e43766d4102568f4d
jresun <jresun@163.com>
2018-04-23 18:15:44 +0800

    fixed 鼠标手势

--- js/append.js
+++ js/append.js
@@ -14,102 +14,99 @@ window.onload = function () {
             document.body.appendChild(style);
         });
     })();
-};
+    (function () {
+        var isMouseDown   = false,
+            startPosition = {},
+            endPosition   = {},
+            isRight       = false,
+            isBottom      = false,
+            timer         = null,
+            body          = document.querySelector('body'),
+            fixedDiv      = document.createElement('div'),
+            appendContent = document.createElement('div');
 
-(function () {
-    var isMouseDown   = false,
-        startPosition = {},
-        endPosition   = {},
-        isRight       = false,
-        isBottom      = false,
-        timer         = null,
-        body          = document.querySelector('body'),
-        fixedDiv      = document.createElement('div'),
-        appendContent = document.createElement('div');
-
-    fixedDiv.style.cssText      = 'position:fixed;left:0;right:0;top:0;bottom:0;background:transparent;';
-    appendContent.style.cssText = 'position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);border-radius:5px;background:#333;';
-    fixedDiv.style.display      = 'none';
-
-    fixedDiv.appendChild(appendContent);
-    body.appendChild(fixedDiv);
-
-    document.addEventListener('mousemove', function (e) {
-        if (e.buttons == 2) {
-            fixedDiv.style.display = 'block';
-            endPosition            = {
+        fixedDiv.style.cssText      = 'position:fixed;left:0;right:0;top:0;bottom:0;background:transparent;';
+        appendContent.style.cssText = 'position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);border-radius:5px;background:#333;';
+        fixedDiv.appendChild(appendContent);
+        body.appendChild(fixedDiv);
+        fixedDiv.style.display = 'none';
+        document.addEventListener('mousemove', function (e) {
+            if (e.buttons == 2) {
+                fixedDiv.style.display = 'block';
+                endPosition            = {
+                    x: e.clientX,
+                    y: e.clientY,
+                }
+                isMouseDown            = false;
+                // 向右
+                if (endPosition.x - startPosition.x > 50 && !isRight) {
+                    var img = document.createElement('img');
+                    img.src = chrome.extension.getURL('img/right.png');
+                    appendContent.appendChild(img);
+                    isRight = true;
+                }
+                // 向下
+                if (endPosition.y - startPosition.y > 50 && !isBottom) {
+                    var img = document.createElement('img');
+                    img.src = chrome.extension.getURL('img/down.png');
+                    appendContent.appendChild(img);
+                    isBottom = true;
+                }
+                clearTimeout(timer);
+                timer = setTimeout(function () {
+                    mouseUpEvent();
+                }, 2400);
+            }
+        }, false);
+        document.addEventListener('mousedown', function (e) {
+            if (e.button != 2) {
+                isMouseDown = false;
+                return;
+            }
+            startPosition = {
                 x: e.clientX,
                 y: e.clientY,
             }
-            isMouseDown            = false;
-            // 向右
-            if (endPosition.x - startPosition.x > 50 && !isRight) {
-                var img = document.createElement('img');
-                img.src = chrome.extension.getURL('img/right.png');
-                appendContent.appendChild(img);
-                isRight = true;
-            }
-            // 向下
-            if (endPosition.y - startPosition.y > 50 && !isBottom) {
-                var img = document.createElement('img');
-                img.src = chrome.extension.getURL('img/down.png');
-                appendContent.appendChild(img);
-                isBottom = true;
-            }
-            clearTimeout(timer);
-            timer = setTimeout(function () {
-                mouseUpEvent();
-            }, 2400);
-        }
-    }, false);
-    document.addEventListener('mousedown', function (e) {
-        if (e.button != 2) {
-            isMouseDown = false;
-            return;
-        }
-        startPosition = {
-            x: e.clientX,
-            y: e.clientY,
-        }
-        isMouseDown   = true;
-    }, false);
+            isMouseDown   = true;
+        }, false);
 
-    function removeContextmenu(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        return false;
-    }
-
-    function mouseUpEvent() {
-        // 同时为假,直接return
-        if (!isBottom && !isRight) {
-            return;
+        function removeContextmenu(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            return false;
         }
-        document.addEventListener('contextmenu', removeContextmenu, false);
-        chrome.extension.sendRequest({
-            event: isBottom && isRight ? 'close' : 'update',
-            url  : location.href
-        }, function (response) {
-            appendContent.innerHTML = '';
-            fixedDiv.style.display  = 'none';
-            if (!response) {
+
+        function mouseUpEvent() {
+            // 同时为假,直接return
+            if (!isBottom && !isRight) {
                 return;
             }
-            if (response.event == 'update') {
-                location.href = location.href;
-                document.removeEventListener('contextmenu', removeContextmenu, false);
-            }
-        });
-        isBottom = isRight = false;
-    }
-
-    document.addEventListener('mouseup', function (e) {
-        if (e.button != 2) {
-            return;
+            document.addEventListener('contextmenu', removeContextmenu, false);
+            chrome.extension.sendRequest({
+                event: isBottom && isRight ? 'close' : 'update',
+                url  : location.href
+            }, function (response) {
+                appendContent.innerHTML = '';
+                fixedDiv.style.display  = 'none';
+                if (!response) {
+                    return;
+                }
+                if (response.event == 'update') {
+                    location.href = location.href;
+                    document.removeEventListener('contextmenu', removeContextmenu, false);
+                }
+            });
+            isBottom = isRight = false;
         }
-        mouseUpEvent();
-        e.stopPropagation();
-        e.preventDefault();
-        return false;
-    }, false);
-})();
+
+        document.addEventListener('mouseup', function (e) {
+            if (e.button != 2) {
+                return;
+            }
+            mouseUpEvent();
+            e.stopPropagation();
+            e.preventDefault();
+            return false;
+        }, false);
+    })();
+};

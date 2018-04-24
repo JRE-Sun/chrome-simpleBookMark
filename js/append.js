9861ed53ec4c3e31a7032e47587b4210958deda1
jresun <jresun@163.com>
2018-04-24 15:37:07 +0800

    window.onload->document.ready

--- js/append.js
+++ js/append.js
@@ -1,4 +1,35 @@
-window.onload = function () {
+(function () {
+    var ie  = !!(window.attachEvent && !window.opera);
+    var wk  = /webkit\/(\d+)/i.test(navigator.userAgent) && (RegExp.$1 < 525);
+    var fn  = [];
+    var run = function () {
+        for (var i = 0; i < fn.length; i++) fn[i]();
+    };
+    var d   = document;
+    d.ready = function (f) {
+        if (!ie && !wk && d.addEventListener)
+            return d.addEventListener('DOMContentLoaded', f, false);
+        if (fn.push(f) > 1) return;
+        if (ie)
+            (function () {
+                try {
+                    d.documentElement.doScroll('left');
+                    run();
+                }
+                catch (err) {
+                    setTimeout(arguments.callee, 0);
+                }
+            })();
+        else if (wk)
+            var t = setInterval(function () {
+                if (/^(loaded|complete)$/.test(d.readyState))
+                    clearInterval(t), run();
+            }, 0);
+    };
+})();
+
+
+document.ready(function () {
     (function () {
         function iGetInnerText(testStr) {
             var resultStr = testStr.replace(/\ +/g, ""); // 去掉空格
@@ -27,6 +58,14 @@ window.onload = function () {
 
         fixedDiv.style.cssText      = 'z-index:9999999;position:fixed;left:0;right:0;top:0;bottom:0;background:transparent;';
         appendContent.style.cssText = 'position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);border-radius:5px;background:#333;display: flex;padding: 10px;align-items: center;justify-content: center;';
+        var rightImg                = document.createElement('img');
+        rightImg.src                = chrome.extension.getURL('img/right.png');
+        var leftImg                 = document.createElement('img');
+        leftImg.src                 = chrome.extension.getURL('img/down.png');
+        rightImg.style.display      = 'none';
+        leftImg.style.display       = 'none';
+        appendContent.appendChild(leftImg);
+        appendContent.appendChild(rightImg);
         fixedDiv.appendChild(appendContent);
         body.appendChild(fixedDiv);
         fixedDiv.style.display = 'none';
@@ -41,18 +80,14 @@ window.onload = function () {
                 }
                 isMouseDown = false;
                 // 向右
-                if (endPosition.x - startPosition.x > 50 && !isRight) {
-                    var img = document.createElement('img');
-                    img.src = chrome.extension.getURL('img/right.png');
-                    appendContent.appendChild(img);
-                    isRight = true;
+                if (endPosition.x - startPosition.x > 30 && !isRight) {
+                    rightImg.style.display = 'block';
+                    isRight                = true;
                 }
                 // 向下
-                if (endPosition.y - startPosition.y > 50 && !isBottom) {
-                    var img = document.createElement('img');
-                    img.src = chrome.extension.getURL('img/down.png');
-                    appendContent.appendChild(img);
-                    isBottom = true;
+                if (endPosition.y - startPosition.y > 30 && !isBottom) {
+                    leftImg.style.display = 'block';
+                    isBottom              = true;
                 }
                 clearTimeout(timer);
                 timer = setTimeout(function () {
@@ -85,8 +120,15 @@ window.onload = function () {
                 return;
             }
             document.addEventListener('contextmenu', removeContextmenu, false);
+            var event = '';
+            if (isBottom) {
+                event = 'update';
+            }
+            if (isBottom && isRight) {
+                event = 'close';
+            }
             chrome.extension.sendRequest({
-                event: isBottom && isRight ? 'close' : 'update',
+                event: event,
                 url  : location.href
             }, function (response) {
                 appendContent.innerHTML = '';
@@ -112,4 +154,4 @@ window.onload = function () {
             return false;
         }, false);
     })();
-};
+});

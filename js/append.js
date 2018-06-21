c145d94141127c07fb48fc52ba3097745ca0cb76
Sun Jre <jresun@163.com>
2018-06-21 16:53:02 +0800

    增加特殊站点屏蔽

--- js/append.js
+++ js/append.js
@@ -1,71 +1,73 @@
-(function () {
-    var ie  = !!(window.attachEvent && !window.opera);
-    var wk  = /webkit\/(\d+)/i.test(navigator.userAgent) && (RegExp.$1 < 525);
-    var fn  = [];
-    var run = function () {
+(function() {
+    var ie = !!(window.attachEvent && !window.opera);
+    var wk = /webkit\/(\d+)/i.test(navigator.userAgent) && (RegExp.$1 < 525);
+    var fn = [];
+    var run = function() {
         for (var i = 0; i < fn.length; i++) fn[i]();
     };
-    var d   = document;
-    d.ready = function (f) {
+    var d = document;
+    d.ready = function(f) {
         if (!ie && !wk && d.addEventListener)
             return d.addEventListener('DOMContentLoaded', f, false);
         if (fn.push(f) > 1) return;
         if (ie)
-            (function () {
+            (function() {
                 try {
                     d.documentElement.doScroll('left');
                     run();
-                }
-                catch (err) {
+                } catch (err) {
                     setTimeout(arguments.callee, 0);
                 }
             })();
         else if (wk)
-            var t = setInterval(function () {
+            var t = setInterval(function() {
                 if (/^(loaded|complete)$/.test(d.readyState))
                     clearInterval(t), run();
             }, 0);
     };
 })();
 
-document.ready(function () {
-    (function () {
+document.ready(function() {
+    var isHasSpecialSite = location.href.search(/zq/ig) > -1
+    if (isHasSpecialSite) return;
+    (function() {
         function iGetInnerText(testStr) {
             var resultStr = testStr.replace(/\ +/g, ""); // 去掉空格
-            resultStr     = testStr.replace(/[ ]/g, "");    // 去掉空格
-            resultStr     = testStr.replace(/[\r\n]/g, ""); // 去掉回车换行
-            resultStr     = testStr.replace(/(?:^|\n|\r)\s*\/\/.*(?:\r|\n|$)/g, ""); // 去掉注释
+            resultStr = testStr.replace(/[ ]/g, ""); // 去掉空格
+            resultStr = testStr.replace(/[\r\n]/g, ""); // 去掉回车换行
+            resultStr = testStr.replace(/(?:^|\n|\r)\s*\/\/.*(?:\r|\n|$)/g, ""); // 去掉注释
             return resultStr;
         }
 
-        chrome.storage.local.get('value', function (valueArray) {
-            var style       = document.createElement('style');
+        chrome.storage.local.get('value', function(valueArray) {
+            console.log(valueArray, 1);
+            var style = document.createElement('style');
             style.innerText = iGetInnerText(valueArray.value || '');
             document.body.appendChild(style);
         });
     })();
-    (function () {
+    (function() {
         var startPosition = {},
-            endPosition   = {},
-            isRight       = false,
-            isBottom      = false,
-            isLeft        = false,
-            timer         = null,
-            body          = document.querySelector('body'),
-            fixedDiv      = document.createElement('div'),
+            endPosition = {},
+            isRight = false,
+            isBottom = false,
+            isLeft = false,
+            timer = null,
+            body = document.querySelector('body'),
+            fixedDiv = document.createElement('div'),
             appendContent = document.createElement('div');
 
-        fixedDiv.style.cssText      = 'z-index:9999999;position:fixed;left:0;right:0;top:0;bottom:0;background:transparent;';
+        fixedDiv.style.cssText = 'z-index:9999999;position:fixed;left:0;right:0;top:0;bottom:0;background:transparent;';
         appendContent.style.cssText = 'position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);border-radius:5px;background:#333;display: flex;padding: 10px;align-items: center;justify-content: center;';
-        var rightImg                = document.createElement('img');
-        rightImg.src                = chrome.extension.getURL('img/right.png');
-        var bottomImg               = document.createElement('img');
-        bottomImg.src               = chrome.extension.getURL('img/down.png');
-        var leftImg                 = document.createElement('img');
-        leftImg.src                 = chrome.extension.getURL('img/right.png');
-        rightImg.style.cssText      = 'display:none;margin:0!important;padding:0!important;';
-        bottomImg.style.cssText     = 'display:none;margin:0!important;padding:0!important;';
-        leftImg.style.cssText       = 'display:none;margin:0!important;padding:0!important;transform:rotateY(180deg);';
+        var rightImg = document.createElement('img');
+        rightImg.src = chrome.extension.getURL('img/right.png');
+        var bottomImg = document.createElement('img');
+        bottomImg.src = chrome.extension.getURL('img/down.png');
+        var leftImg = document.createElement('img');
+        leftImg.src = chrome.extension.getURL('img/right.png');
+        rightImg.style.cssText = 'display:none;margin:0!important;padding:0!important;';
+        bottomImg.style.cssText = 'display:none;margin:0!important;padding:0!important;';
+        leftImg.style.cssText = 'display:none;margin:0!important;padding:0!important;transform:rotateY(180deg);';
         appendContent.appendChild(leftImg);
         appendContent.appendChild(bottomImg);
         appendContent.appendChild(rightImg);
@@ -82,7 +84,7 @@ document.ready(function () {
         }
 
         var startTime = new Date().getTime();
-        document.addEventListener('mousemove', function (e) {
+        document.addEventListener('mousemove', function(e) {
             var endTime = new Date().getTime();
             if (e.buttons == 2 && endTime - startTime > 27) {
                 startTime = endTime;
@@ -96,21 +98,21 @@ document.ready(function () {
                 // 向右
                 if (endPosition.x - startPosition.x > 30 && !isRight) {
                     rightImg.style.display = 'block';
-                    isRight                = true;
+                    isRight = true;
                 }
                 // 向左
                 if ((startPosition.x > endPosition.x + 30) && !isLeft) {
                     console.log(startPosition.x, endPosition.x);
                     leftImg.style.display = 'block';
-                    isLeft                = true;
+                    isLeft = true;
                 }
                 // 向下
                 if (endPosition.y - startPosition.y > 30 && !isBottom) {
                     bottomImg.style.display = 'block';
-                    isBottom                = true;
+                    isBottom = true;
                 }
                 clearTimeout(timer);
-                timer = setTimeout(function () {
+                timer = setTimeout(function() {
                     mouseUpEvent();
                 }, 2400);
             }
@@ -118,15 +120,15 @@ document.ready(function () {
 
         // 双击次数默认为0
         var rightMenuClickNums = 0,
-            cliclTimer         = null,
-            isMac              = detectOS();
+            cliclTimer = null,
+            isMac = detectOS();
         // 当前是mac
         if (isMac == 'Mac') {
             // 是mac那么进来直接取消右键菜单,改成双击右键出现菜单
             document.addEventListener('contextmenu', removeContextmenu, false);
         }
 
-        document.addEventListener('mousedown', function (e) {
+        document.addEventListener('mousedown', function(e) {
             if (e.button != 2) {
                 return;
             }
@@ -137,7 +139,7 @@ document.ready(function () {
                     rightMenuClickNums = 0;
                 }
                 clearTimeout(cliclTimer);
-                cliclTimer = setTimeout(function () {
+                cliclTimer = setTimeout(function() {
                     document.addEventListener('contextmenu', removeContextmenu, false);
                     rightMenuClickNums = 0;
                 }, 300);
@@ -160,13 +162,13 @@ document.ready(function () {
             if (!isBottom && !isRight && !isLeft) {
                 return;
             }
-            rightImg.style.display  = 'none';
+            rightImg.style.display = 'none';
             bottomImg.style.display = 'none';
-            leftImg.style.display   = 'none';
+            leftImg.style.display = 'none';
             document.addEventListener('contextmenu', removeContextmenu, false);
             if (isBottom && !isLeft && !isRight) {
                 location.href = location.href;
-                isBottom      = isRight = isLeft = false;
+                isBottom = isRight = isLeft = false;
                 document.removeEventListener('contextmenu', removeContextmenu, false);
                 return;
             }
@@ -183,14 +185,13 @@ document.ready(function () {
             if (isBottom && isRight && !isLeft) {
                 chrome.extension.sendRequest({
                     event: 'close',
-                    url  : location.href
-                }, function (response) {
-                });
+                    url: location.href
+                }, function(response) {});
             }
             isBottom = isRight = isLeft = false;
         }
 
-        document.addEventListener('mouseup', function (e) {
+        document.addEventListener('mouseup', function(e) {
             if (e.button != 2) {
                 return;
             }
